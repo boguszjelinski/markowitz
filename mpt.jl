fc3599ae02f251e35c98e4c1fb7a0ed9bc4ddd2a
Bogusz Jelinski <bogusz.jelinski@gmail.com>
2019-11-26 14:01:11 +0100

    new syntax for Julia

--- mpt.jl
+++ mpt.jl
@@ -2,13 +2,13 @@ using JuMP
 using Ipopt
 N=4
 r_min=0.035
-m = Model()
+m = Model(solver=IpoptSolver())
 C = [0.076900 0.039968 0.018111 -0.000288 ; 0.039968 0.050244 0.019033 -0.000060 ; 0.018111 0.019033 0.021381 0.007511 ; -0.000288 -0.000060 0.007511 0.008542]
 Mean = [0.0073 0.0346 0.0444 0.0271]
 @variable(m, 0 <= x[i=1:N] <= 1)
-@objective(m,Min,sum{x[j]*sum{x[i]*C[i,j],i=1:N},j=1:N})
-@constraint(m, sum{x[i]*Mean[i],i=1:N} >=r_min)
-@constraint(m, sum{x[i],i=1:N} ==1.0)
+@objective(m,Min,sum(x[j] * sum(x[i] * C[i,j] for i = 1:N) for j = 1:N))
+@constraint(m, sum(x[i] * Mean[i] for i = 1:N) >=r_min)
+@constraint(m, sum(x[i] for i = 1:N) ==1.0)
 status = solve(m)
 for i= 1:N
     println(getvalue(x[i]))
